fef12928838a19a540a1af3c3afe468388e9ce4a
Parbez <74945038+imranbarbhuiya@users.noreply.github.com>
2021-12-16 10:47:46 +0530

    final commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import turtle
+import pandas
+from numpy import any
 
 screen = turtle.Screen()
 screen.title("U.S. States game")
@@ -6,12 +8,32 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 
 turtle.shape(image)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < len(all_states):
+    answer_state = screen.textinput(f"{len(guessed_states)}/{len(all_states)} state guessed", "What's the another "
+                                                                                              "state name'?").title()
+
+    if answer_state == "Exit":
+        break
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+        if answer_state not in guessed_states:
+            guessed_states.append(answer_state)
 
 
-def get_mouse_click_cord(x, y):
-    print(x, y)
+states_to_learn = list(set(all_states) - set(guessed_states))
+# print(f"You guessed {len(guessed_states)} states")
+# print(f"You need to learn {len(states_to_learn)} states")
+# print(states_to_learn)
 
-
-turtle.onscreenclick(get_mouse_click_cord)
-
-turtle.mainloop()
+new_data = pandas.DataFrame(states_to_learn)
+new_data.to_csv("states_to_learn.csv")
